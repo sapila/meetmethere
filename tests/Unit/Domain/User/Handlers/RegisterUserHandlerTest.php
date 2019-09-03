9d4db878265640b6dc52c8d0bf74a552f001afcd
Nikos Gkogktzilas <nik.gkogk@hotmail.com>
2019-09-03 12:02:18 +0200

    failure test

--- tests/Unit/Domain/User/Handlers/RegisterUserHandlerTest.php
+++ tests/Unit/Domain/User/Handlers/RegisterUserHandlerTest.php
@@ -7,12 +7,11 @@ use App\Domain\User\Messages\RegisterUser;
 use App\Domain\User\Messages\UserRegistered;
 use App\Dto\UserDto;
 use App\Repositories\UserRepository;
+use Exception;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Hash;
 use PHPUnit\Framework\MockObject\MockObject;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterUserHandlerTest extends TestCase
 {
@@ -64,5 +63,16 @@ class RegisterUserHandlerTest extends TestCase
         Event::assertDispatched(UserRegistered::class, function ($e) {
             return $e->user->getId() === 5;
         });
+    }
+
+    public function test_handler_does_not_emit_event_if_exceptions_is_trown()
+    {
+        $this->expectException(Exception::class);
+        $this->userRepository->method('createUser')->willThrowException(new Exception('DB not reachable'));
+
+        $handler = new RegisterUserHandler($this->userRepository);
+        $handler->handle($this->event);
+
+        Event::assertNotDispatched(UserRegistered::class);
     }
 }
